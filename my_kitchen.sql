1e2f64cb360064107dca2965c03dfdcc8aac872e
Farhaan Beeharry <farhaanbeeharry.ms@gmail.com>
2022-07-20 01:01:16 +0400

    sql dump

--- my_kitchen.sql
+++ my_kitchen.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 19, 2022 at 10:50 PM
--- Server version: 10.4.24-MariaDB
+-- Generation Time: Jul 19, 2022 at 11:00 PM
+-- Server version: 10.5.9-MariaDB
 -- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -46,6 +46,18 @@ CREATE TABLE `recipe` (
   `taste` int(11) NOT NULL,
   `type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `recipe`
+--
+
+INSERT INTO `recipe` (`id`, `name`, `description`, `taste`, `type`) VALUES
+(1, 'Moussaka', 'Moussaka is an eggplant- or potato-based dish, often including ground meat, which is common in the Balkans and the Middle East, with many local and regional variations.', 1, 2),
+(2, 'Rasgulla', 'Rasgulla, also known in Rasagola, Rosogola or Rosogolla, is a syrupy dessert popular in the Indian subcontinent and regions with South Asian diaspora.', 0, 2),
+(3, 'Rasmalai', 'Ras malai, rasamalai, or rossomalai, or is a dessert originating from the eastern regions of the Indian subcontinent. ', 0, 1),
+(4, 'Kokokrunch', 'Kokokrunch is a chocolate-flavored whole-grain breakfast cereal distributed by Nestlé in most of Europe, Asia, the Middle-East and Latin America. ', 0, 0),
+(5, 'Omelette', 'In cuisine, an omelette is a dish made from beaten eggs, fried with butter or oil in a frying pan.', 1, 0),
+(6, 'Biryani', 'Biryani is a mixed rice dish originating among the royal khansamas of the durbar of Old Delhi, under the Mughal Empire, during the late 16th century of the then Mughal Court.', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -114,7 +126,7 @@ ALTER TABLE `ingredient`
 -- AUTO_INCREMENT for table `recipe`
 --
 ALTER TABLE `recipe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `recipe_ingredient`
